415e6f76aad0a918b9f7ef5315ff1bbd23259afd
Nicolás Rodriguez <nicolasrod@gmail.com>
2024-10-01 17:36:41 -0300

    Quickstart docs Auth initialization script

---
+++ docs/quickstart.md
@@ -0,0 +1,18 @@
+# Quickstart
+
+- Clone the AnemicPHP repo:
+
+```
+$ git clone https://github.com/nicolasrod/anemicphp myprojectname
+$ cd myprojectname
+$ php -S 0.0.0.0:8000
+```
+
+- Load the Auth initialization script. Go to this URL: `http://0.0.0.0:8000/init`
+- Go to the homepage: `http://0.0.0.0:8000`
+- Click on Login. Now you can log as:
+
+|Username | Password |
+|admin@local.host | admin1234 |
+|user@local.host| user1234 |
+
---
+++ pages/init.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use Anemic\{Db, Users, Roles, Config};
+
+$ok = Db::RunInTx(function ($db) {
+    // Create USERS and ROLES tables in Auth DB
+    $db->exec(<<<SQL
+        create table if not exists users (
+            id integer primary key autoincrement, 
+            email,
+            firstname,
+            lastname,
+            password_hash,
+            unique(email)
+        );
+        create unique index users_email on users(email);
+        create table if not exists roles (
+            id integer primary key autoincrement,
+            id_user integer,
+            name,
+            unique(id_user, name),
+            foreign key(id_user) REFERENCES users(id)
+        );
+        create unique index roles_users on roles(id_user, name);
+    SQL);
+
+    Users::Add($db, "admin@local.host", "admin1234", "Local", "Administrator");
+    Users::Add($db, "user@local.host", "user1234", "Local", "User");
+    Roles::Grant($db, "admin@local.host", "admin");
+}, Config::Get("auth_database"));
+
+if (! $ok) {
+    $error = Db::GetLastError();
+    echo "Error: {$error->getMessage()} ({$error->getFile()} - {$error->getLine()}";
+} else {
+    echo "- Auth DB created!";
+}
